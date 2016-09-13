cf89e17e90761589e6c3696860791966e02b0f84
s tech <email@sarcas.tech>
2016-09-13 14:17:33 -0400

    minor refactor for file paths

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 let path = require('path')
+let process = require('process')
 
 function* applyRoutes (list) {
   for (let item of list) {
@@ -8,15 +9,17 @@ function* applyRoutes (list) {
 }
 
 function loadApp (express, callback) {
+  let cwd = process.cwd()
   for (let module of applyRoutes(this.moduleWhiteList)) {
-    express.use(`/${module.replace(this.homeDirectory, '')}`, require(path.join(this.applicationPath, module)))
+    let resolvedPath = path.resolve(cwd, this.applicationPath, module)
+    express.use(`/${module.replace(this.homeDirectory, '')}`, require(resolvedPath))
   }
   callback()
 }
 
 function Xploader (config) {
   this.moduleWhiteList = config.whiteList || []
-  this.applicationPath = config.applicationPath || `../app`
+  this.applicationPath = config.applicationPath || `./app`
   this.homeDirectory = config.home || 'index'
 
   return loadApp.bind(this)
